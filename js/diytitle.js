3db7e48217b55b92e7c397c79a7954d5dffdf463
iyuyi <xuqingdian2021@163.com>
2023-12-27 14:16:16 +0800

    Site updated: 2023-12-27 14:15:57

--- js/diytitle.js
+++ js/diytitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = 'w(ﾟДﾟ)w';
+        document.title = '可是意难平又如何呢？';
         clearTimeout(titleTime);
     }
     else {
         //返回当前页面时标签显示内容
-        document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
+        document.title = '明知道不可能，才一再地说。人总是这样。' + OriginTitile;
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
